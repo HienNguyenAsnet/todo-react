2b16c9140823dc290f7510b061ae9e45330a1ad6
Hien Nguyen <hien.nguyenvan@asnet.com.vn>
2020-03-03 10:18:57 +0700

    Updates

---
+++ precache-manifest.6be9e1b939a42254037b8ebeceb1a0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2848078d3d8e7631916b8b0828d056c9",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "c01ce80d966afe19b9af",
+    "url": "/todo-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04cee7080f812e61b73b",
+    "url": "/todo-react/static/js/2.2094c4c0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo-react/static/js/2.2094c4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01ce80d966afe19b9af",
+    "url": "/todo-react/static/js/main.0483f081.chunk.js"
+  },
+  {
+    "revision": "8c8404ddc91811c2534b",
+    "url": "/todo-react/static/js/runtime-main.c528bf7d.js"
+  }
+]);
